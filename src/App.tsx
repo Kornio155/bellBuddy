c2ac04134d7112118dd3de5530fe0543521a79de
Kornel <korneljaniuk@gmail.com>
2025-06-01 19:23:58 +0200

    pierwzy commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Zegar from './zegar.tsx';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Mój zegar</h1>
+            <Zegar />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/zegar.tsx
@@ -0,0 +1,23 @@
+﻿import { useEffect, useState } from 'react';
+
+function Zegar() {
+    const [time, setTime] = useState(new Date());
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    const formattedTime = time.toLocaleTimeString();
+
+    return (
+        <div style={{ fontSize: '2rem', fontFamily: 'monospace' }}>
+            🕒 {formattedTime}
+        </div>
+    );
+}
+
+export default Zegar;
